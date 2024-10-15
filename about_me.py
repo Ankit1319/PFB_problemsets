779ba6e5c1d3ca957d7cd1744c6e9c4b32d2ac1b
pfb2024 <pfb2024@info06.cshl.edu>
2024-10-15 11:55:02 -0400

    My updated python script

--- about_me.py
+++ about_me.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
+import sys
 
 print("My name is 'Ankit Sharma'")
+
+print ("'I love blue color.' 'What about you'")
+
+My_fav_activity = "I love to sleep"
+print (My_fav_activity)
+
+print('I love dogs''What about you?')
+
+print(f"What's your name")
+
+My_name = sys.argv[1]
+My_fav_color = sys.argv[2]
+My_fav_activity = sys.argv[3]
+My_fav_animal = sys.argv[4]
+
+print ('My name is', My_name, 'I love', My_fav_color, 'color', 'My fav activity is', My_fav_activity, 'And i love my', My_fav_animal)
